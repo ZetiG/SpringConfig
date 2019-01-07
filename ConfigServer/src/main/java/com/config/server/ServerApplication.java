545e2eae3cc1e49a821091cf91a97db897b62a49
张孟轲 <zhangmk@none.com>
2019-01-07 22:48:55 +0800

    高可用配置中心

--- ConfigServer/src/main/java/com/config/server/ServerApplication.java
+++ ConfigServer/src/main/java/com/config/server/ServerApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @EnableDiscoveryClient //Eureka-server支持
 @EnableConfigServer //SpringConfig-server支持
 @SpringBootApplication
